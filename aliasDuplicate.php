5afa9578f880fe178e95c0468844db9a203ead73
citypeople <hi@citypeople.ru>
2017-08-03 03:56:02 +0500

    Update aliasDuplicate.php
    Обновление скрипта для php 7.0

--- aliasDuplicate.php
+++ aliasDuplicate.php
@@ -1,10 +1,10 @@
 <?php
 error_reporting(0);
-mysql_connect('localhost','','');
-mysql_select_db('');
+$connect = mysqli_connect('localhost','db_uname','db_pass','db_name');
 
-$d = mysql_query("SELECT group_concat(`id`) ids, count(id) c FROM `revo_site_content` GROUP by concat(uri) HAVING c > 1");
-while($res = mysql_fetch_array($d))
+
+$d = mysqli_query($connect, "SELECT group_concat(`id`) ids, count(id) c FROM `revo_site_content` GROUP by concat(uri) HAVING c > 1");
+while($res = mysqli_fetch_array($d))
 {
 $ids = explode(",",$res['ids']);
 $i = 0;
@@ -14,12 +14,12 @@ foreach($ids as $id)
 	if($i == 1)
 		continue;
 	echo $id . "\r\n<br/>";
-	$m = mysql_fetch_array(mysql_query("SELECT uri FROM `revo_site_content` WHERE id = $id"));
+	$m = mysqli_fetch_array(mysqli_query($connect, "SELECT uri FROM `revo_site_content` WHERE id = $id"));
 	$uri = $m['uri'];
 	$uri2 = explode("/",$uri);
 	$uri2 = end($uri2);
 	$uri = str_replace($uri2,$id."-".$uri2, $uri);
 		
-	mysql_query("UPDATE `revo_site_content` SET `uri` = '".$uri."', `alias` = CONCAT('".$id."-',`alias`) WHERE `id` = '$id'");
+	mysqli_query($connect, "UPDATE `revo_site_content` SET `uri` = '".$uri."', `alias` = CONCAT('".$id."-',`alias`) WHERE `id` = '$id'");
 }
 }
